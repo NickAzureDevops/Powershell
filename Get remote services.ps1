15db902619ce4f190402bd85511115b500ea656f
Nicholas Chang <60232417+nicholaschangIT@users.noreply.github.com>
2020-02-29 14:41:16 +0000

    Changes made

--- Get remote services.ps1
+++ Get remote services.ps1
@@ -12,11 +12,17 @@ $Service = Get-service -ComputerName $Computername
 
 # Foreach to perform action on each objects in $service
 
-Foreach ($service in $services {
+Foreach ($service in $services) {
 
     #create a varible containing status and displayname using member enumeration
 
-    $Service
+    $ServiceStatus = $service.status
+    $ServiceDisplayName = $Service.displayname
 
-
-}
+    if ($ServiceStatus -eq 'Running') {
+        Write-Output "Service OK - Status of $ServiceDisplayName is $ServiceStatus"
+    }
+    Else { 
+        Write-Output "Check Service - Status of $ServiceDisplayName is $ServiceStatus"
+    }
+    }
