afa89231d4856517bd7f0474a303ba6d27c591c8
Nicholas Chang <60232417+nicholaschangIT@users.noreply.github.com>
2020-02-29 19:03:13 +0000

    Changes made

--- Learning.ps1
+++ Learning.ps1
@@ -1,7 +1,6 @@
 $computerName = 'IPD19298'
 $credentials = Get-Credential
 
-
 $cimsession = New-CimSession -ComputerName $computerName -Credential $credentials
 
 $cimsession 
@@ -17,7 +16,6 @@ Write-Output $stoppedservices
 $computername = read-host "Please Enter Computer Name"
 Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computername |
 Select-Object -Property CSName,LastBootupTime
-
 
 #Add variable to invoke- $data = invoke-command -Computer $computerName -Credentials $credentials -scriptblock {get-service -ComputerName $using:ComputerName} need to use $using variable in order to be read into the script block
 
