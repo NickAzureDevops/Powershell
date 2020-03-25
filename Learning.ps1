df7cd3aef57e90f9c7f59037c526fb4436901ba0
Nicholas Chang <60232417+nicholaschangIT@users.noreply.github.com>
2020-03-25 20:44:57 +0000

    commit

--- Learning.ps1
+++ Learning.ps1
@@ -15,7 +15,3 @@ $computername = read-host "Please Enter Computer Name"
 Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computername | Select-Object -Property CSName,LastBootupTime
 
 #Add variable to invoke- $data = invoke-command -Computer $computerName -Credentials $credentials -scriptblock {get-service -ComputerName $using:ComputerName} need to use $using variable in order to be read into the script block
-
-#invoke-command -computername dc01 -cred (get-credentials) -scriptblock {Get-aduser -identity nicholas.chang | format-list}
-
-# Parameterized script
