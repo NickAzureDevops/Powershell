5729740d70ca3a73c93ed71b1495b62e2e3d4f6a
Nicholas Chang <60232417+nicholaschangIT@users.noreply.github.com>
2020-03-25 16:55:08 +0000

    changes made to the try-catch

--- Install Software remotely.ps1
+++ Install Software remotely.ps1
@@ -9,17 +9,18 @@ $TempDirectory = C:\temp
 #copy item from network share to local computer in temp folder. 
 foreach ($computer in $computers) 
 {
+
 try {
     Copy-Item -Path $installer -Destination $TempDirectory -Force  
     }
-catch {
-
+    
+    catch { "An error occurred." } 
+    
     If ($TempDirectory = $False) 
     {New-Item -ItemType Directory -Force -Path C:\Temp #create new directory if folder do not exist
          {
             #Install the application from temp folder 
             Invoke-Command -ComputerName $computers.name -ScriptBlock $TempDirectory -ArgumentList "--quiet" -Verb RunAs -Wait -NoNewWindow }
-          
+            Remove-Item $TempDirectory
         }
-    }   
-}   
+}  
