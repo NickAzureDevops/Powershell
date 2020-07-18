bbe6f9e8654f59b8a15f10bfd8c78501cdea3e02
nicholasc001@hotmail.com <Nicholasc001@hotmail.com>
2020-07-19 00:53:03 +0100

    Latest Changes

--- Check window version.ps1
+++ Check window version.ps1
@@ -1,7 +1,11 @@
-Import-Module activedirectory
+#Import-Module activedirectory
 
-#Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,OperatingSystemVersion,ipv4Address |
- #Export-CSV ADcomputerslist.csv | Select-Object OperatingSystemVersion 
+Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,OperatingSystemVersion,ipv4Address,ReleaseId
+Export-CSV ADcomputerslist.csv | Select-Object OperatingSystemVersion 
 
-Get-Item -Path HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProductName\
+(Get-Item -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").getvalue('ReleaseID') 
+
 #HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ReleaseId
+
+Get-ADComputer -Filter * -Properties Name, OperatingSystem, OperatingSystemVersion |
+Select-Object Name, OperatingSystem, OperatingSystemVersion | Sort-Object Name | Format-Table 
