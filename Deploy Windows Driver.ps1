f551809a7a52db1ab250a55c7fd41c18558ec781
Nicholas Chang <60232417+nicholaschangIT@users.noreply.github.com>
2020-07-18 14:48:18 +0100

    test

--- Deploy Windows Driver.ps1
+++
@@ -1,4 +0,0 @@
-#deployed inf windows driver from folder. 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-Get-ChildItem "\\fsp-sr1\Departmental\Software\Hewlett Packard\x64" -Recurse -Filter "*inf"| 
-ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install } 
