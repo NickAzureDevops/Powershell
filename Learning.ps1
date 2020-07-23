324c253143b2e3b0d54c64540f96cb097d04ab30
nicholasc001@hotmail.com <Nicholasc001@hotmail.com>
2020-07-23 21:55:58 +0100

    change made

--- Learning.ps1
+++ Learning.ps1
@@ -13,3 +13,11 @@ $computername = read-host "Please Enter Computer Name"
 Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computername | Select-Object -Property CSName,LastBootupTime
 
 #Add variable to invoke- $data = invoke-command -Computer $computerName -Credentials $credentials -scriptblock {get-service -ComputerName $using:ComputerName} need to use $using variable in order to be read into the script block
+
+Get-Alias Get-ChildItem
+Set-ADAccountPassword jfrost -NewPassword
+Set-ADUser jfrost -ChangePasswordAtLogon $True - changed at login 
+get-aduser -filter "enabled -eq 'false'" - Or I could pipe in a bunch of users and delete them with one simple command:
+-property WhenChanged -SearchBase "OU=Employees,
+etsn -ComputerName
+Get-service b* | where-Object {$_.DisplayName -match “.+user”}
