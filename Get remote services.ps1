af0f6dbe6fdada28f278e73310c25a542dce4601
nicholasc001@hotmail.com <Nicholasc001@hotmail.com>
2020-07-24 18:47:36 +0100

    changes made

--- Get remote services.ps1
+++ Get remote services.ps1
@@ -1,39 +1,15 @@
-#get-services 
 Param ( 
-        
         [Parameter(Mandatory=$true)]
         [string[]]    # [] After the string accepts multiple inputs 
-        $Computername
-        
+        $Computername  
     )
-        
-       #  get-service -ComputerName $Computername | 
-       #  where-object -Property Status -eq 'Stopped'
-       # for each 
-      foreach ($target in $Computername) {
-        
+
+    foreach ($target in $Computername) 
+    {
         get-service -ComputerName $Computername | 
-        
-        where-object -Property Status -eq 'Stopped'
-}
-
-
-# Foreach to perform action on each objects in $service
-
-Foreach ($service in $services) {
-
-    #create a varible containing status and displayname using member enumeration
-
-    $ServiceStatus = $service.status
-    $ServiceDisplayName = $Service.displayname
-
-    if ($ServiceStatus -eq 'Running') {
-        Write-Output "Service OK - Status of $ServiceDisplayName is $ServiceStatus"
+        Where-Object -Property Status -eq 'Stopped'
     }
-    Else { 
-        Write-Output "Check Service - Status of $ServiceDisplayName is $ServiceStatus"
-    }
-    }
-# find all the stopped services 
+    
+#     find all the stopped services 
 #     $Computername = "Client02"
-  #   get-service -ComputerName $Computername | Where-Object -Property Status -eq 'Stopped' 
+#   get-service -ComputerName $Computername | Where-Object -Property Status -eq 'Stopped' 
