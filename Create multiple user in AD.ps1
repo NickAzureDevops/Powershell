5422d7303395b9cdd1f28b63672eb932d3c5bea4
nicholasc001@hotmail.com <Nicholasc001@hotmail.com>
2020-07-25 22:30:54 +0100

    changes added

--- Create multiple user in AD.ps1
+++ Create multiple user in AD.ps1
@@ -1,9 +1,9 @@
-﻿#Import Active Directory 
-#$AddGroup = Users
+﻿Import-Module ActiveDirectory
 
-Import-Module ActiveDirectory
+#import from CSV data    
+$Users = Import-Csv -Path "c:\Users\nicholas.chang\Documents\Scripts\newusers.csv" 
 
-$Users = Import-Csv -Path "c:\Users\nicholas.chang\Documents\Scripts\newusers.csv"            
+#Loop through each row containing user details in the CSV file
 foreach ($User in $Users)            
 {            
     $Displayname = $User.FirstName + " " + $User.Lastname            
@@ -16,8 +16,11 @@ foreach ($User in $Users)
     $Description = $User.Description
     $department = $User.department 
     $jobtitle   = $User.jobtitle
+    $group = $User.$group
     $Password = $User.Password  
     
+    # Creating User with AD Attributes
+
     New-ADUser `
     -Name $Displayname -DisplayName $Displayname -SamAccountName $SAM -UserPrincipalName "$SAM@cloudtechgenius.com" `
     -GivenName $UserFirstname -Surname $UserLastname -Department $department `
@@ -25,6 +28,13 @@ foreach ($User in $Users)
     -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true `
     -Path "$OU"  -ChangePasswordAtLogon $true  –PasswordNeverExpires $false 
    
-     Write-Host $User.SAM "created successfully"
- #   Add-ADGroupMember -Identity $group -Members $AddGroup
+     Write-Host $User.SAM "created successfully" | 
+     Write-Host $User.SAM "Please Wait adding groups membership"
+    
+    # Add-ADGroupMember -Identity $group -Members $Group
+
+    # assigned user a office365 licence 
+
+
+
 }
