08bc73620ad0edb393d485aab65f5eaf938b122c
nicholasc001@hotmail.com <Nicholasc001@hotmail.com>
2020-07-26 14:07:23 +0100

    Add User to Group

--- Create multiple user in AD.ps1
+++ Create multiple user in AD.ps1
@@ -1,37 +1,34 @@
 ﻿Import-Module ActiveDirectory
-
 #import from CSV data    
 $Users = Import-Csv -Path "c:\Users\nicholas.chang\Documents\Scripts\newusers.csv" 
 
 #Loop through each row containing user details in the CSV file
-foreach ($User in $Users)            
-{            
+foreach ($User in $Users) {            
     $Displayname = $User.FirstName + " " + $User.Lastname            
     $UserFirstname = $User.Firstname            
     $UserLastname = $User.Lastname            
     $OU = $User.OU           
     $SAM = $User.SAM      
     $email = $User.$email  
-    $telephone = $User.$telephone
+    $telephone  = $User.telephone
     $Description = $User.Description
     $department = $User.department 
-    $jobtitle   = $User.jobtitle
-    $group = $User.$group
+    $jobtitle = $User.jobtitle
+    $groupname = $User.$groupname
     $Password = $User.Password  
     
     # Creating User with AD Attributes
 
     New-ADUser `
-    -Name $Displayname -DisplayName $Displayname -SamAccountName $SAM -UserPrincipalName "$SAM@cloudtechgenius.com" `
-    -GivenName $UserFirstname -Surname $UserLastname -Department $department `
-    -Description $Description -EmailAddress $User.email -Title $jobtitle -OfficePhone $telephone `
-    -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true `
-    -Path "$OU"  -ChangePasswordAtLogon $true  –PasswordNeverExpires $false 
+        -Name $Displayname -DisplayName $Displayname -SamAccountName $SAM -OfficePhone $telephone  `
+        -UserPrincipalName "$SAM@cloudtechgenius.com" -GivenName $UserFirstname -Surname $UserLastname -Department $department `
+        -Description $Description -EmailAddress $User.email -Title $jobtitle  `
+        -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true `
+        -Path "$OU"  -ChangePasswordAtLogon $true  –PasswordNeverExpires $false 
    
-     Write-Host $User.SAM "created successfully" | 
-     Write-Host $User.SAM "Please Wait adding groups membership"
-    
-    # Add-ADGroupMember -Identity $group -Members $Group
-
-    # assigned user a office365 licence 
-}
+        Write-Host $User.SAM "created successfully" 
+        # Add user to Group
+        Add-ADGroupMember $User.groupname -Members $SAM 
+        # Add user to mutiple groups 
+        Write-Host $User.SAM "Please Wait adding groups membership"
+    }
